a042398c87f68e87ca9446b44f08638917b460e9
Nicklas Vejlgaard <qzw@live.dk>
2021-10-04 09:31:13 +0200

    Revert "fix frontend issues and add auth middleware"
    This reverts commit 373604f0913abacf820ed0d3f99cf3480424df67.

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::resource('recipe', RecipeController::class)->middleware(['auth']);
+Route::resource('recipe', RecipeController::class);
 
 require __DIR__.'/auth.php';
