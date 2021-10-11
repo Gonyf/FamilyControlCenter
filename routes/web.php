5de37398383ad6e00f263ddd060ebc9869e9233c
Nicklas Vejlgaard <qzw@live.dk>
2021-10-11 22:22:00 +0200

    added auth middleware to recipe routes

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,18 @@
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
-Route::resource('recipe', RecipeController::class);
+    Route::resource('recipe', RecipeController::class);
+});
+
+
 
 require __DIR__.'/auth.php';
