b23da1419fc0d97c2b4aa7793b50a4dfbaafad73
Nicklas Vejlgaard <qzw@live.dk>
2021-10-12 00:05:14 +0200

    small fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        {{ $headScritps }}
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         {{ $headScripts ?? ''}}
